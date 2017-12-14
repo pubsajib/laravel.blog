eacdc654ad0cb535a15be07ec63026375045d89d
pubsajib <pubsajib@gmail.com>
2017-12-15 03:05:58 +0600

    Comment model added

--- app/Comment.php
+++ app/Comment.php
@@ -4,10 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model
-{
+class Comment extends Model {
     protected $table = 'comments';
-    
+
 	public function post() {
 		return $this->belongsTo(Post::class);
 	}
